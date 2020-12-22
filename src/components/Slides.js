50dcc138c570f77f2c627e7fdd04c37fca31e1f8
docmu <docm@mymail.vcu.edu>
2020-12-22 18:44:53 -0500

    solved

--- src/components/Slides.js
+++ src/components/Slides.js
@@ -1,21 +1,24 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-function Slides({slides}) {
+export default class Slides extends Component {
+    render() {
+        const item = this.props.slides[this.props.index];
+        const index = this.props.index;
 
-    return (
-        <div>
-            <div id="navigation" className="text-center">
-                <button data-testid="button-restart" className="small outlined">Restart</button>
-                <button data-testid="button-prev" className="small">Prev</button>
-                <button data-testid="button-next" className="small">Next</button>
+        return (
+            <div>
+                <div id="navigation" className="text-center">
+                    <button onClick={this.props.resetSlides} data-testid="button-restart" className="small outlined">Restart</button>
+                    <button onClick={this.props.prevSlide} disabled={index === 0} data-testid="button-prev" className="small">Prev</button>
+                    <button onClick={this.props.nextSlide} disabled={index === this.props.slides.length - 1} data-testid="button-next" className="small">Next</button>
+                </div>
+                <div id="slide" className="card text-center">
+                    <h1 data-testid="title">{item.title}</h1>
+                    <p data-testid="text">{item.text}</p>
+                </div>
             </div>
-            <div id="slide" className="card text-center">
-                <h1 data-testid="title">Slide Title Here</h1>
-                <p data-testid="text">Slide Text Here</p>
-            </div>
-        </div>
-    );
-
+        );
+    }
 }
 
-export default Slides;
+
